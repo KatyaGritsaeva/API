e96974d7807d3d8bd5b2105af37bcc380ea27efc
KatyaGritsaeva <gekaterina2019@gmail.com>
2022-07-24 16:12:00 +0300

    first commit

---
+++ src/main/java/FormDate.java
@@ -0,0 +1,6 @@
+public class FormDate {
+    //Класс для объявления переменных в дате рождения
+    public int day;
+    public int mounth;
+    public int year;
+}
---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public String name; // поле для имени
+    public String passport; //  "4444 № 44444444"
+    public String patronymic; // "Иванович"
+    public String phone; // "+7 (999)-999-99-99"
+    public String surname; // "Иванов"
+    public String subscription; //True
+    FormDate birthday = new FormDate(); //Обвление переменной birthday, что ее нужно брать из класса FormDate
+}
